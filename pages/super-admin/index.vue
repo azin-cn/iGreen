1e784aa3b22579067d20acc6262a8067273ebf3e
azin-cn <luckywords@foxmail.com>
2022-08-27 01:14:47 +0800

    超级管理员页面

---
+++ pages/super-admin/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <view class="flex justify-center align-center">SuperAdmin</view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss"></style>
