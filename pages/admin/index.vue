70b0daafc0a441f82e3a71e57da5db45743d35eb
azin-cn <luckywords@foxmail.com>
2022-08-27 01:14:06 +0800

    普通管理员页面

---
+++ pages/admin/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <view class="flex justify-center align-center">Admin</view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss"></style>
